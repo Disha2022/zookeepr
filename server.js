ce368ea80a44d49ea9557ecf4fd524cb5bc3a42b
Disha2022 <99584117+Disha2022@users.noreply.github.com>
2022-04-26 01:44:59 -0400

    Add Heroku

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require("express"); // require express
+const { animals } = require("./data/animals");
+const app = express(); //instantiate the server
+//===============================
+function filterByQuery(query, animalsArray) {
+  let filteredResults = animalsArray;
+  if (query.personalityTraits) {
+    if (typeof query.personalityTraits === "string") {
+      personalityTraitsArray = [query.personalityTraits];
+    } else {
+      personalityTraitsArray = query.personalityTraits;
+    }
+    personalityTraitsArray.forEach((trait) => {
+      filteredResults = filteredResults.filter(
+        (animal) => animal.personalityTraits.indexOf(trait) !== -1
+      );
+    });
+  }
+
+  if (query.diet) {
+    filteredResults = filteredResults.filter(
+      (animal) => animal.diet === query.diet
+    );
+  }
+  if (query.species) {
+    filteredResults = filteredResults.filter(
+      (animal) => animal.species === query.species
+    );
+  }
+  if (query.name) {
+    filteredResults = filteredResults.filter(
+      (animal) => animal.name === query.name
+    );
+  }
+  return filteredResults;
+}
+//route==describes the route the client will have to fetch from
+app.get("/api/animals", (req, res) => {
+  let results = animals;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
+app.listen(3001, () => {
+  console.log(`API Server now on port 3001!`); // where is the port from?
+});
